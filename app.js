f6595204991db759fb052ab944f0d266c356d9c8
DevSatish2725 <mauryasatish146@gmail.com>
2026-05-19 12:35:07 +0530

    Fitst commit

---
+++ app.js
@@ -0,0 +1,20 @@
+// const { sum } = require("./calculate/sum");
+// const multiply3  = require("./calculate/multiply");
+
+const { sum, multiply3 } = require("./calculate");
+// require steps
+// 1. it check file type
+// 2. load file content
+// 3. wraps inside an IIFE
+// 4. Evaluation(in this step module.exports added in the IIFE)
+// 5. Caching (it chaches file so that if same file used in multiple modules that will
+// n't run again and again)
+
+var a = 10;
+var b = 20;
+
+sum(a, b);
+
+multiply3(a, b);
+
+const URI = "mongodb+srv://mauryasatish146:GlGtPNf8roUYgkyD@cluster0.4xq3d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
---
+++ calculate/index.js
@@ -0,0 +1,4 @@
+const { sum } = require("./sum");
+const multiply3 = require("./multiply");
+
+module.exports = { sum, multiply3 };
